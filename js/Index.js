a2a233669e171249c99c153b89b16338514e4c59
Nigel A <nigel@bgcreatives.com>
2013-03-21 21:22:33 -0400

    1st round site deployment
    basic site throw up.  Still needs dev pics and email funcationality
    should be checked

--- js/Index.js
+++ js/Index.js
@@ -1,98 +1,104 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
+$(".showImage").hover(function () {
+    "use strict";
+    $(".placeImage").addClass("imgHover");
+    $("imgHover").css({
+        "width": "450px",
+        "height": "150px"
+    });
+}, function () {
+    "use strict";
+    $(".placeImage").removeClass("imgHover");
+});
 
-	$(document).ready(function() {
-	    $('.top-bar').waypoint('sticky', {
-	        wrapper: '<div class="sticky-wrapper" />',
-	        stuckClass: 'fixie'
-	    });
-		
-		function addColor(w,z) {
-			var x = ".meetus > a, .contact > a, .work > a, .about > a"
-			var y = "blue"
-			$(w).waypoint(function() {
-				$(x).removeClass(y);
-				$(z).addClass(y);
-			});
-		}
-		addColor(".about", ".about > a");
-		addColor("#work", ".work > a");
-		addColor("#meetus", ".meetus > a");
-		addColor("#contact", ".contact > a");
-			
-	    function scroll(x, y) {
-	        $(x).on("click", function(event) {
-	            event.preventDefault();
-	            console.log("Hello World");
-	            $(y).ScrollTo();
-	        })
-	    }
-	    scroll(".work", "#work");
-	    scroll(".meetus", "#meetus");
-	    scroll(".contact", "#contact");
-	    
-	    imgRotator();
-	    
-	    $(".imgHover").hover(function imgHover() {
-	        var imgTitle = $(this).next("img").attr("title");
-	        var imgHeight = $(this).next("img").css("height");
-	        var imgWidth = $(this).next("img").css("width");
-	        /* $('#txtRotate').text('Cool Kids'); */
-	        $('.imgHover').css({
-	            'height': imgHeight,
-	            'width': imgWidth
-	        });
-	        $(this).stop().animate({
-	            opacity: .7
-	        }, "slow").append('<h3 class="biggie">' + imgTitle + '</h3>');
-	    }, function() {
-	        $(this).stop().animate({
-	            opacity: 0
-	        }, "slow").children().remove();
-	    });
-	});
-	
-	$(".showImage").hover(function() {
-	    console.log("hello world");
-	    $(".placeImage").addClass("imgHover");
-	    $("imgHover").css({
-	        "width": "450px",
-	        "height": "150px"
-	    })
-	}, function() {
-	    $(".placeImage").removeClass("imgHover");
-	});
+function phraseloop() {
+    "use strict";
+    var phrases = ['Creatives', 'Nerds', 'Researchers', 'Technology Driven'
+    , 'Motivated', 'Ambitious', 'Focused', 'Dorky', 'Forever Learning'];
+    for (var i = 0; i < phrases.length; i++) {
+        $('<span/>', {
+            class: 'textItem',
+            text: phrases[i]
+        }).appendTo('#txtRotate');
+    }
+}
 
-	function imgRotator(){
-	/*--- builds phrases dynamically in divs on load ---*/
-		phraseloop();
-	/*--- Image Rotation Magic ---*/	
-		if($('.textItem').length > 0)
-	     {
-	         $('.textItem:first').addClass('current').fadeIn(1000);
-	         setInterval('textRotate()', 3000);
-	     }
-	 }
-	 
-     function textRotate() {
-     
-         var current = $('#txtRotate > .current');
-         if(current.next().length == 0)
-         {
-             current.removeClass('current').fadeOut(1000);
-             $('.textItem:first').addClass('current').fadeIn(1000);
-         }
-         else
-         {
-             current.removeClass('current').fadeOut(1000);
-             current.next().addClass('current').fadeIn(1000);
-         }	
-      }
-		/*--- Array of Phrases to add to the rotation and the loop to push the phrases into seperate spans ---*/
-	 function phraseloop(){
-		var phrases = ['Creatives', 'Nerds', 'Researchers', 'Technology Driven', 'Motivated', 'Ambitious', 'Focused', 'Dorky', 'Forever Learning'];
-		
-		for (var i=0; i < phrases.length; i++){
-		  $('<span/>', {class: 'textItem', text: phrases[i] }).appendTo('#txtRotate');
-	 	}
-	 }
+function imgRotator() {
+    "use strict";
+    phraseloop(); /*--- Image Rotation Magic ---*/
+    if ($('.textItem').length > 0) {
+        $('.textItem:first').addClass('current').fadeIn(1000);
+        setInterval('textRotate()', 3000);
+    }
+}
+
+function textRotate() {
+    var current = $('#txtRotate > .current');
+    if (current.next().length == 0) {
+        current.removeClass('current').fadeOut(1000);
+        $('.textItem:first').addClass('current').fadeIn(1000);
+    } else {
+        current.removeClass('current').fadeOut(1000);
+        current.next().addClass('current').fadeIn(1000);
+    }
+}
+
+$(document).ready(function () {
+    "use strict";
+    $('.top-bar').waypoint('sticky', {
+        wrapper: '<div class="sticky-wrapper" />',
+        stuckClass: 'fixie'
+    });
+
+    function addColor(w, z) {
+        var x = ".meetus > a, .contact > a, .work > a, .about > a",
+            y = "blue";
+        $(w).waypoint(function () {
+            $(x).removeClass(y);
+            $(z).addClass(y);
+        });
+    }
+    addColor(".about", ".about > a");
+    addColor("#work", ".work > a");
+    addColor("#meetus", ".meetus > a");
+    addColor("#contact", ".contact > a");
+
+    function scroll(x, y) {
+        $(x).on("click", function (event) {
+            event.preventDefault();
+            $(y).ScrollTo();
+        });
+    }
+    scroll(".work", "#work");
+    scroll(".meetus", "#meetus");
+    scroll(".contact", "#contact");
+    imgRotator();
+    $(".imgHover").hover(function imgHover() {
+        var imgTitle, imgHeight, imgWidth;
+        imgTitle = $(this).next("img").attr("title");
+        imgHeight = $(this).next("img").css("height");
+        imgWidth = $(this).next("img").css("width");
+        $('.imgHover').css({
+            'height': imgHeight,
+            'width': imgWidth
+        });
+        $(this).stop().animate({
+            opacity: 0.7
+        }, "slow").append('<h3 class="biggie">' + imgTitle + '</h3>');
+    }, function () {
+        $(this).stop().animate({
+            opacity: 0
+        }, "slow").children().remove();
+    });
+
+    $('#slideShim').cycle({ 
+		fx:     'fade',
+		speed:  1500,
+		timeout: 4000,
+		prev:   '#back',
+		next:   '#forward',
+		pause:  1,
+		pager:  '#pager'
+	});
+});
